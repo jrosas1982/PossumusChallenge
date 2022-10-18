fba2985d8d4aa51f29e6d6d19561af4641a3ba90
Juan Pablo <jrosas1982@gmail.com>
2022-10-18 01:03:11 -0300

    Configuracion y Uso de serilog

--- PossumusChallenge/Controllers/v1/EmpleadoController.cs
+++ PossumusChallenge/Controllers/v1/EmpleadoController.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Logging;
 using Swashbuckle.AspNetCore.Annotations;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Serilog;
+using System;
 
 namespace API.PossumusChallenge.Controllers.v1
 {
@@ -19,7 +21,6 @@ namespace API.PossumusChallenge.Controllers.v1
         private readonly ICandidatoService _candidatoService;
         private readonly IEmpleoService _empleoService;
         private readonly IMapper _mapper;
-        private readonly ILogger _logger;
 
         public EmpleadoController(ICandidatoService candidatoService, IMapper mapper , IEmpleoService empleoService)
         {
@@ -29,66 +30,127 @@ namespace API.PossumusChallenge.Controllers.v1
         }
         [HttpGet("GetCandidatos")]
         [SwaggerOperation("Retorna todos los candidatos")]
-        public async Task<IEnumerable<CandidatoDto>> Get()
+        public async Task<IActionResult> Get()
         {
-            var result = await _candidatoService.GetCandidatos();
-            var empleados = _mapper.Map<IEnumerable<CandidatoDto>>(result.Data);
-            return empleados;
+            Log.Information("Obteniendo datos de GetCandidatos");
+            try
+            {
+                var result = await _candidatoService.GetCandidatos();
+                var empleados = _mapper.Map<IEnumerable<CandidatoDto>>(result.Data);
+                return Ok(empleados);
+            }
+            catch (Exception ex)
+            {
+                Log.Warning($"Error obteniendo datos de GetCandidatos msj: {ex.Message}");
+                return NotFound();
+            }
+        
         }
         [HttpGet("GetCandidatoById/{Id:int}")]
         [SwaggerOperation("Retorna un candidato según el Id especificado")]
         public async Task<IActionResult> GetCandidatoById(int Id)
         {
-            var result = await _candidatoService.GetCandidatoById(Id);
-            if (result.Data == null) {
-                return NotFound($"No hay candidato con Id: {Id}");
+            Log.Information("Obteniendo datos de GetCandidatos");
+            try
+            {
+                var result = await _candidatoService.GetCandidatoById(Id);
+                if (result.Data == null)
+                {
+                    Log.Warning($"No hay candidato con Id: {Id}");
+                    return NotFound($"No hay candidato con Id: {Id}");
+                }
+                var empleados = _mapper.Map<CandidatoDto>(result.Data);
+                return Ok(empleados);
             }
-            var empleados = _mapper.Map<CandidatoDto>(result.Data);
-            return Ok(empleados);
+            catch (Exception ex)
+            {
+                Log.Warning($"Error obteniendo datos de GetCandidatoById msj: {ex.Message}");
+                return BadRequest();
+            }
+    
         }
         [HttpPost("CrearEmpleado")]
         [SwaggerOperation("Crea un Empleado")]
         public async Task<IActionResult> CrearEmpleado([FromBody] CandidatoCreacionDto candidato)
         {
-            var empleado = _mapper.Map<Candidato>(candidato);
-            var result = await _candidatoService.CrearCandidato(empleado);
-            if (result.Data)
-                return Ok();
-            return BadRequest($"Error al crear el empleado : {result.Message} ");
+            Log.Information("Intentando CrearEmpleado");
+            try
+            {
+                var empleado = _mapper.Map<Candidato>(candidato);
+                var result = await _candidatoService.CrearCandidato(empleado);
+                if (result.Data)
+                    return Ok();
+                return BadRequest($"Error al crear el empleado : {result.Message} ");
+
+            }
+            catch (Exception ex)
+            {
+                Log.Warning($"Error obteniendo datos de GetCandidatoById msj: {ex.Message}");
+                return BadRequest($"Error al crear el empleado : {ex.Message} ");
+            }
+     
         }
         [HttpPut("UpdateCandidato/{Id:int}")]
         [SwaggerOperation("Actualiza el empleado especificado por el parametro candidatoId")]
         public async Task<IActionResult> UpdateCandidato([FromBody] CandidatoBaseDto candidato, int Id)
         {
-            var empleado = await _candidatoService.GetCandidatoById(Id);
-            var empleadoMapp = _mapper.Map(candidato , empleado.Data);
-            var result = await _candidatoService.UpdateCandidato(empleadoMapp);
-            if (result.Data)
-                return Ok();
-            return BadRequest($"Error al actualizar el cine : {result.Message} ");
+            Log.Information("Intentando UpdateCandidato");
+            try
+            {
+                var empleado = await _candidatoService.GetCandidatoById(Id);
+                var empleadoMapp = _mapper.Map(candidato, empleado.Data);
+                var result = await _candidatoService.UpdateCandidato(empleadoMapp);
+                if (result.Data)
+                    return Ok();
+                return BadRequest($"Error al UpdateCandidato : {result.Message} ");
+            }
+            catch (Exception ex)
+            {
+                Log.Warning($"Error UpdateCandidato con msj: {ex.Message}");
+                return BadRequest($"Error al UpdateCandidato : {ex.Message} ");
+            }
+
         }
         [HttpPatch("UpdateParcialEmpleado/{Id:int}")]
         [SwaggerOperation("Actualiza algunos datos del empleado especificado por el parametro candidatoId")]
         public async Task<IActionResult> Patch([FromBody] JsonPatchDocument<CandidatoActualizacionDto> candidatoPatch, int Id )
         {
-            if (candidatoPatch == null) {
-                return BadRequest();
+            Log.Information(messageTemplate: "Intentando UpdateParcialEmpleado");
+            try
+            {
+                if (candidatoPatch == null)
+                {
+                    Log.Warning($"Error UpdateParcialEmpleado BadRequest");
+                    return BadRequest();
+                }
+                Log.Debug("Intentando buscar el candidato");
+                var empleadoDB = await _candidatoService.GetCandidatoById(Id);
+                if (empleadoDB == null)
+                {
+                    Log.Warning($"no existe el candidato");
+                    return NotFound();
+                }
+                Log.Debug("Intentando Mappeo del candidato");
+                var empleadoMappDTO = _mapper.Map<CandidatoActualizacionDto>(empleadoDB.Data);
+                candidatoPatch.ApplyTo(empleadoMappDTO, ModelState);
+                if (!ModelState.IsValid)
+                {
+                    Log.Warning($"ModelState invalido");
+                    return BadRequest(ModelState);
+                }
+                Log.Debug("Intentando segundo Mappeo del candidato");
+                var candidatoMapp = _mapper.Map(empleadoMappDTO, empleadoDB.Data);
+                var result = await _candidatoService.UpdateCandidato(candidatoMapp);
+                if (result.Data)
+                    return Ok();
+                return BadRequest($"Error al actualizar el candidato : {result.Message} ");
             }
-            var empleadoDB = await _candidatoService.GetCandidatoById(Id);
-            if (empleadoDB == null) { 
-                return NotFound();
+            catch (Exception ex)
+            {
+                Log.Warning($"Error UpdateParcialEmpleado con msj: {ex.Message} ");
+                return BadRequest($"Error al actualizar el candidato : {ex.Message} ");
             }
 
-            var empleadoMappDTO = _mapper.Map<CandidatoActualizacionDto>(empleadoDB.Data);
-            candidatoPatch.ApplyTo(empleadoMappDTO, ModelState);
-            if (!ModelState.IsValid) {
-                return BadRequest(ModelState);
-            }
-            var candidatoMapp = _mapper.Map(empleadoMappDTO, empleadoDB.Data);
-            var result = await _candidatoService.UpdateCandidato(candidatoMapp);
-            if (result.Data)
-                return Ok();
-            return BadRequest($"Error al actualizar el cine : {result.Message} ");
         }
         [HttpDelete("DeleteCandidato/{Id:int}")]
         [SwaggerOperation("Elimina el empleado especificado por el parametro Id")]
--- PossumusChallenge/Program.cs
+++ PossumusChallenge/Program.cs
@@ -2,8 +2,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,11 +15,17 @@ namespace PossumusChallenge
     {
         public static void Main(string[] args)
         {
+            string path = Directory.GetCurrentDirectory();
+            string pathlog = path + @"\Loggeos\Log.txt";
+            Log.Logger = new LoggerConfiguration().MinimumLevel.Information()
+                .WriteTo.File(pathlog, outputTemplate:"{Timestamp:yyyy-MM-dd HH:mm:ss} {Message:lj}{NewLine}{Exeption}").CreateLogger();
+                
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
